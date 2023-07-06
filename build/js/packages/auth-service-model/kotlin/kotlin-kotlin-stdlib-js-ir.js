08094dce1e9d3096c1da6afbd3b0e789911155ff
timmson <timmson666@mail.ru>
2023-07-07 01:18:53 +0300

    Add Kotlin Multiplatform and shared module

---
+++ build/js/packages/auth-service-model/kotlin/kotlin-kotlin-stdlib-js-ir.js
@@ -0,0 +1,139 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd)
+    define(['exports'], factory);
+  else if (typeof exports === 'object')
+    factory(module.exports);
+  else
+    root['kotlin-kotlin-stdlib-js-ir'] = factory(typeof this['kotlin-kotlin-stdlib-js-ir'] === 'undefined' ? {} : this['kotlin-kotlin-stdlib-js-ir']);
+}(this, function (_) {
+  'use strict';
+  //region block: pre-declaration
+  setMetadataFor(Unit, 'Unit', objectMeta);
+  //endregion
+  function Unit() {
+    Unit_instance = this;
+  }
+  var Unit_instance;
+  function Unit_getInstance() {
+    if (Unit_instance == null)
+      new Unit();
+    return Unit_instance;
+  }
+  function implement(interfaces) {
+    var maxSize = 1;
+    var masks = [];
+    var inductionVariable = 0;
+    var last = interfaces.length;
+    while (inductionVariable < last) {
+      var i = interfaces[inductionVariable];
+      inductionVariable = inductionVariable + 1 | 0;
+      var currentSize = maxSize;
+      var tmp1_elvis_lhs = i.prototype.$imask$;
+      var imask = tmp1_elvis_lhs == null ? i.$imask$ : tmp1_elvis_lhs;
+      if (!(imask == null)) {
+        masks.push(imask);
+        currentSize = imask.length;
+      }
+      var iid = i.$metadata$.iid;
+      var tmp;
+      if (iid == null) {
+        tmp = null;
+      } else {
+        // Inline function 'kotlin.let' call
+        // Inline function 'kotlin.contracts.contract' call
+        // Inline function 'kotlin.js.implement.<anonymous>' call
+        tmp = bitMaskWith(iid);
+      }
+      var iidImask = tmp;
+      if (!(iidImask == null)) {
+        masks.push(iidImask);
+        currentSize = Math.max(currentSize, iidImask.length);
+      }
+      if (currentSize > maxSize) {
+        maxSize = currentSize;
+      }
+    }
+    return compositeBitMask(maxSize, masks);
+  }
+  function bitMaskWith(activeBit) {
+    var intArray = new Int32Array((activeBit >> 5) + 1 | 0);
+    var numberIndex = activeBit >> 5;
+    var positionInNumber = activeBit & 31;
+    var numberWithSettledBit = 1 << positionInNumber;
+    intArray[numberIndex] = intArray[numberIndex] | numberWithSettledBit;
+    return intArray;
+  }
+  function compositeBitMask(capacity, masks) {
+    var tmp = 0;
+    var tmp_0 = new Int32Array(capacity);
+    while (tmp < capacity) {
+      var tmp_1 = tmp;
+      var result = 0;
+      var inductionVariable = 0;
+      var last = masks.length;
+      while (inductionVariable < last) {
+        var mask = masks[inductionVariable];
+        inductionVariable = inductionVariable + 1 | 0;
+        if (tmp_1 < mask.length) {
+          result = result | mask[tmp_1];
+        }
+      }
+      tmp_0[tmp_1] = result;
+      tmp = tmp + 1 | 0;
+    }
+    return tmp_0;
+  }
+  function protoOf(constructor) {
+    return constructor.prototype;
+  }
+  function defineProp(obj, name, getter, setter) {
+    return Object.defineProperty(obj, name, {configurable: true, get: getter, set: setter});
+  }
+  function objectCreate(proto) {
+    return Object.create(proto);
+  }
+  function classMeta(name, associatedObjectKey, associatedObjects, suspendArity) {
+    return createMetadata('class', name, associatedObjectKey, associatedObjects, suspendArity, null);
+  }
+  function createMetadata(kind, name, associatedObjectKey, associatedObjects, suspendArity, iid) {
+    var undef = VOID;
+    return {kind: kind, simpleName: name, associatedObjectKey: associatedObjectKey, associatedObjects: associatedObjects, suspendArity: suspendArity, $kClass$: undef, iid: iid};
+  }
+  function setMetadataFor(ctor, name, metadataConstructor, parent, interfaces, associatedObjectKey, associatedObjects, suspendArity) {
+    if (!(parent == null)) {
+      ctor.prototype = Object.create(parent.prototype);
+      ctor.prototype.constructor = ctor;
+    }
+    var metadata = metadataConstructor(name, associatedObjectKey, associatedObjects, suspendArity == null ? [] : suspendArity);
+    ctor.$metadata$ = metadata;
+    if (!(interfaces == null)) {
+      var receiver = !(metadata.iid == null) ? ctor : ctor.prototype;
+      receiver.$imask$ = implement(interfaces);
+    }
+  }
+  function objectMeta(name, associatedObjectKey, associatedObjects, suspendArity) {
+    return createMetadata('object', name, associatedObjectKey, associatedObjects, suspendArity, null);
+  }
+  function get_VOID() {
+    _init_properties_void_kt__3zg9as();
+    return VOID;
+  }
+  var VOID;
+  var properties_initialized_void_kt_e4ret2;
+  function _init_properties_void_kt__3zg9as() {
+    if (!properties_initialized_void_kt_e4ret2) {
+      properties_initialized_void_kt_e4ret2 = true;
+      VOID = void 0;
+    }
+  }
+  //region block: exports
+  _.$_$ = _.$_$ || {};
+  _.$_$.a = classMeta;
+  _.$_$.b = protoOf;
+  _.$_$.c = setMetadataFor;
+  _.$_$.d = VOID;
+  //endregion
+  return _;
+}));
+
+//# sourceMappingURL=kotlin-kotlin-stdlib-js-ir.js.map
