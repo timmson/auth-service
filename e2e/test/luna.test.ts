f9b6c7830b220ceb5cbe576dea1ae9a16d5eb033
timmson <timmson666@mail.ru>
2024-03-08 18:20:25 +0300

    Add Playwright

--- e2e/test/luna.test.ts
+++ e2e/test/luna.test.ts
@@ -4,8 +4,10 @@ test("Should insert valid card number", async ({page}) => {
 	await page.goto(".")
 
 	await page.getByLabel("Card number").pressSequentially("2200770212727079", {delay: 100})
-
 	await expect(page.getByText("is valid")).toBeVisible()
+
+	await page.getByLabel("Card number").pressSequentially("2200770212727078", {delay: 100})
+	await expect(page.getByText("is invalid")).toBeVisible()
 
 	await page.waitForTimeout(500)
 })
